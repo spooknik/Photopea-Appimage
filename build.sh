7508f9faa732e16f4945d0a7559bf2e6b241eefd
Steven <steven@debian-pc.steven-pc>
2019-01-10 07:11:11 +0100

    update 1

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-nativefier -e 4.0.1 --name "photopea" --icon /home/steven/Pictures/photopea.png "https://www.photopea.com/"
+nativefier -e 4.0.1 --name "photopea" --icon build/photopea.png "https://www.photopea.com/"
 mv ~/photopea-linux-x64 .
 cp build/package.json photopea-linux-x64/resources/app/
 cd photopea-linux-x64/resources/app/
 yarn add electron-builder --dev
 yarn electron-builder
 cp -r dist ../../../
+mv "dist/Photopea 1.0.0.AppImage" dist/Photopea_1.0.0.AppImage
 
